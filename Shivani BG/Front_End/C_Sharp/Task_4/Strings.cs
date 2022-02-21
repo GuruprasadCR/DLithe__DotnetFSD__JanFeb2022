313f15634033c13601d7c50c31e40f3725f521bd
shivanibg44 <99085505+shivanibg44@users.noreply.github.com>
2022-02-21 09:14:48 +0530

    Add files via upload

---
+++ Shivani BG/Front_End/C_Sharp/Task_4/Strings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CSharpStrings
+{
+    class Strings
+    {
+        static void Main(string[] args)
+        {
+            // Define .NET Strings
+            // String of characters
+            System.String authorName = "Shivani BG";
+
+            // String made of an Integer
+            System.String age = "23";
+
+            // String made of a double
+            System.String numberString = "33.23";
+
+            // Write to Console.
+            Console.WriteLine("Name: {0}", authorName);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Number: {0}", numberString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Shivani BG/Front_End/C_Sharp/Task_4/partial_Method.cs
@@ -0,0 +1,26 @@
+﻿class Program  
+   {  
+       static void Main(string[] args)  
+       {  
+           ClassA cls = new ClassA();  
+           cls.SetSalary();  
+       }  
+   }  
+   public partial class ClassA  
+   {  
+       partial void SetData();  
+   }  
+  
+   public partial class ClassA  
+   {  
+       public void SetSalary()  
+       {  
+           SetData();  
+           // Perform other functionality here  
+       }  
+       partial void SetData()  
+       {  
+           Console.Write("This is partial method.");  
+           Console.ReadKey();  
+       }  
+   }  
