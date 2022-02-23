ccfea88f1b7128ea7f122d7baa21e1576270062f
shivanibg44 <shivanibg5sonu@gmail.com>
2022-02-24 02:19:44 +0530

    committ

---
+++ Shivani BG/Front_End/C_Sharp/Task_8/simple.cs
@@ -0,0 +1,13 @@
+using System;
+namespace First
+{
+    class simple
+    {
+       static void Main(string[] args)
+        {
+            double num = 50.1;
+            Console.WriteLine("the value is"+num);
+
+        }
+    }
+} 
